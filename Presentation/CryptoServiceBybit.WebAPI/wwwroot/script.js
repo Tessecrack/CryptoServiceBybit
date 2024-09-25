b89324daf10f97b7e9d78cc9d8484d9580f1ec8d
Tessecrack <deputamandre441@gmail.com>
2024-09-25 21:43:11 +0300

    added real quotes from Bybit

--- Presentation/CryptoServiceBybit.WebAPI/wwwroot/script.js
+++ Presentation/CryptoServiceBybit.WebAPI/wwwroot/script.js
@@ -2,13 +2,12 @@ let currentSymbol;
 let currentTimeframe;
 let currentCategory;
 
-
 let cachedSymbols = {};
 
 const timeframesSelectElement = document.getElementById("timeframesSelect");
 const categoriesSelectElement = document.getElementById("categoriesSelect");
 const symbolsSelectElement = document.getElementById("symbolsSelect");
-
+const tableBodyElement = document.getElementById("quotes-table-body");
 
 categoriesSelectElement.addEventListener("change", async () => {
     const selectedCategory = categoriesSelectElement.options[categoriesSelectElement.selectedIndex];
@@ -21,18 +20,7 @@ timeframesSelectElement.addEventListener("change", () => {
     currentTimeframe = selectedTimeframe.value;
 });
 
-function addOptionToSelectElement(selectElement, option) {
-    const optionElement = document.createElement("option");
-    optionElement.value = option;
-    optionElement.append(option);
-    selectElement.append(optionElement);
-}
-
-async function init() {
-    await getSymbols();
-}
-
-async function getSymbols(category = "spot") {
+async function getSymbols(category) {
     if (cachedSymbols[category]) {
         const list = cachedSymbols[category];
         symbolsSelectElement.innerHTML = "";
@@ -50,6 +38,41 @@ async function getSymbols(category = "spot") {
             list.forEach(e => addOptionToSelectElement(symbolsSelectElement, e.symbol));
         }
     }
+}
+
+function addOptionToSelectElement(selectElement, option) {
+    const optionElement = document.createElement("option");
+    optionElement.value = option;
+    optionElement.append(option);
+    selectElement.append(optionElement);
+}
+
+function updateQuotesTable() {
+    const symbolsInfos = cachedSymbols[currentCategory];
+    if (symbolsInfos.length > 0) {
+        tableBodyElement.innerHTML = "";
+        for (let i = 0; i < symbolsInfos.length; ++i) {
+            const trElement = document.createElement("tr");
+            const symbolNameTdElement = document.createElement("td");
+            const lastPriceTdElement = document.createElement("td");
+
+            const symbolInfo = symbolsInfos[i];
+
+            symbolNameTdElement.append(symbolInfo.symbol);
+            lastPriceTdElement.append(symbolInfo.lastPrice);
+
+            trElement.appendChild(symbolNameTdElement);
+            trElement.appendChild(lastPriceTdElement);
+
+            tableBodyElement.appendChild(trElement);
+        }
+    }
+}
+
+async function init() {
+    currentCategory = "spot";
+    await getSymbols(currentCategory);
+    updateQuotesTable()
 }
 
 init();
